a6527819e1c3be4cc14f8352dbf3e850e0c83d5b
keslley <keslley@nullbug.dev>
2022-06-20 21:41:04 -0300

    fix enumerate

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :book
   validates :note, :comment, presence: true
-
-  # has_enumeration_for :note, class: Note, create_helpers: true, create_scopes: true
 end
